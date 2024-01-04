df43d41de8e873c4235252cd4b52c19caa7c58b2
suhjs <suhjs@markany.com>
2024-01-04 13:26:02 +0900

    first commit

---
+++ class/pages/_app.js
@@ -0,0 +1,24 @@
+//모든 page 에서 하는공통 설정들 여기서 진행
+
+import { ApolloClient ,InMemoryCache , ApolloProvider} from "@apollo/client"
+
+//import '../styles/globals.css'
+export default function App({ Component }) {
+
+      //GraphQL Setting 
+      const client = new ApolloClient({
+        uri: "http://backend-example.codebootcamp.co.kr/graphql",
+        cache: new InMemoryCache() //컴퓨터의 메모리에다가 backend에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
+      })
+
+
+      return (
+        <div>
+                <div>==== 여기는 _app.js 컴포넌트 시작부분 입니다. =====</div>
+                <ApolloProvider client={client}>
+                      <Component /> 
+                </ApolloProvider>
+                <div>==== 여기는 _app.js 컴포넌트 마지막부분 입니다. =====</div>
+         </div>
+      )
+}
--- class/pages/section01/01-01-example/index.js
+++ class/pages/section01/01-01-example/index.js
@@ -1,1 +1,12 @@
-
+export default function 나만의페이지() {
+    
+    //JS
+    const aaa = 3  //자바스크립트 작성하는 공간 
+    return(
+    //html
+        <div>
+            <div>철수</div>
+            <button>클릭하세요 !!</button>
+        </div>
+    )
+}
