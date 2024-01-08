bcd7d9db6eba062d0f64b7783289ccdb78786f99
suhjs <suhjs@markany.com>
2024-01-08 15:09:38 +0900

    commit

--- class/pages/section04/04-05-graphql-mutation-product/index.js
+++ class/pages/section04/04-05-graphql-mutation-product/index.js
@@ -1,7 +1,7 @@
 import { gql, useMutation } from "@apollo/client"
-import { useState } from 'react'
-const 나의그래프큐엘셋팅 = gql`
-        mutation createProduct($seller: String ,  $createProductInput: $CreateProductInput!) {   #변수의 타입 적는 곳
+
+const CREATE_PRODUCT = gql`
+        mutation createProduct($seller: String ,  $createProductInput: CreateProductInput!) {   #변수의 타입 적는 곳
              createProduct(seller: $seller , createProductInput: $createProductInput){ 
             _id
             number
@@ -10,38 +10,24 @@ const 나의그래프큐엘셋팅 = gql`
         }
 `
 export default function GraphqlMutationPage() {
-    const [writer ,setWriter] = useState()
-    const [title ,setTitle] = useState()
-    const [contents ,setContents] = useState()
 
-    const [나의함수] = useMutation(나의그래프큐엘셋팅)
+
+    const [createProduct] = useMutation(CREATE_PRODUCT)
     const onClickSubmit = async () => {
-        const result = await 나의함수({
+        const result = await createProduct({
             variables: {                       //variables 이게 $ 역할을 함. 
-                writer: writer,
-                title: title,
-                contents: contents
+                seller: "훈이",
+                createProductInput: {
+                    name: "마우스",
+                    detail: "비싼 마우스",
+                    price: 3000
+                }
             }
         })
         console.log (result)
     }
-    const onChangeWriter = (event) => {
-        setWriter(event.target.value)
-    }
-
-    const onChangeTitle = (event) => {
-        setTitle(event.target.value)        
-    }
-
-    const onChangeContents = (event) => {
-        setContents(event.target.value)        
-    }
-
     return (
         <div>
-                작성자: <input type="text" onChange={onChangeWriter} />
-                제목: <input type="text" onChange={onChangeTitle} />
-                내용: <input type="text" onChange={onChangeContents} />
                 <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
         </div>
     )
---
+++ class/pages/section05/05-01-static-routing-moved/index.js
@@ -0,0 +1,6 @@
+
+
+export default function StaticRoutingMovedPage() {
+
+    return <div> 게시글 이동이  완료 되었습니다.</div>
+}
